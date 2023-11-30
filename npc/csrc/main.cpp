12526eed908d6c6e9b8890efcbfee243e825fadd
lipenghui <1033957037@qq.com>
2023-11-30 23:23:17 +0800

    connect dual-control switch to nvboard led output.

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,33 +1,23 @@
 #include <cstdlib>
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <assert.h>
-#include <memory.h>
-#include "Vtop.h"
-#include "verilated.h"
-#include "verilated_vcd_c.h"
+#include <nvboard.h>
+#include <Vtop.h>
 
-int main(int argc, char **argv) {
-  const std::unique_ptr<VerilatedContext> contextp(new VerilatedContext);
-  contextp->commandArgs(argc, argv);
-  const std::unique_ptr<Vtop>top (new Vtop{contextp.get()});
-  Verilated::traceEverOn(true);
-  VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace(tfp, 99); // Trace 99 levels of hierarchy (or see below)
-  // tfp->dumpvars(1, "t"); // trace 1 level under "t"
-  tfp->open("obj_dir/top.vcd");
-  while (!contextp->gotFinish()) {
-    contextp->timeInc(1);
-    int a = rand() & 1;
-    int b = rand() & 1;
-    top->a = a;
-    top->b = b;
-    top->eval();
-    tfp->dump(contextp->time());
-    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-    assert(top->f == (a ^ b));
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+static void single_cycle() {
+  dut.a = rand() & 1; 
+  dut.b = rand() & 1;
+  dut.eval();
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
   }
-  tfp->close();
-  return 0;
 }
